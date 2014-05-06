d17b2c7ad551b7ff53db8deb9e17ef33716bf686
Jason Hrycay <jason.hrycay@motorola.com>
2014-05-06 15:53:51 -0500

    esoc: mdm-4x: Keep PON_RESET_N asserted
    When processing the reboot notifier, QC asserts the reset for 300ms and
    then de-asserts it. This causes the modem to reboot and in the case of a
    shutdown, we end up with the modem staying on draining the battery.
    
    Signed-off-by: Jason Hrycay <jason.hrycay@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>

--- drivers/esoc/esoc-mdm-4x.c
+++ drivers/esoc/esoc-mdm-4x.c
@@ -973,8 +973,6 @@ static void mdm_notify(enum esoc_notify notify, struct esoc_clink *esoc)
 		gpio_direction_output(MDM_GPIO(mdm, AP2MDM_SOFT_RESET),
 				!!mdm->soft_reset_inverted);
 		mdelay(300);
-		gpio_direction_output(MDM_GPIO(mdm, AP2MDM_SOFT_RESET),
-				!mdm->soft_reset_inverted);
 		break;
 /*                                                                            */
 	case ESOC_RAMDUMP_DONE :
