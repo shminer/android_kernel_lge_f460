58ae45304849d781a8d8e8608e28dcdfc1674c79
Aviral Gupta <aviralg@codeaurora.org>
2014-05-12 20:17:48 +0530

    ASoC: wcd9xxx: fix the NULL pointer dereference causing the crash
    If the probe of the device is unsuccessful then the wcd9xxx
    structure is unallocated but is not initialized to NULL.
    If some operation like reset comes, it would dereference the wcd9xxx
    structure leading to the target crash.
    Fix by intializing the client data to NULL after it is unallocated.
    
    Signed-off-by: Aviral Gupta <aviralg@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/mfd/wcd9xxx-core.c
+++ drivers/mfd/wcd9xxx-core.c
@@ -1324,6 +1324,7 @@ err_supplies:
 	wcd9xxx_disable_supplies(wcd9xxx, pdata);
 err_codec:
 	kfree(wcd9xxx);
+	dev_set_drvdata(&client->dev, NULL);
 fail:
 	return ret;
 }
@@ -1336,6 +1337,7 @@ static int wcd9xxx_i2c_remove(struct i2c_client *client)
 	wcd9xxx = dev_get_drvdata(&client->dev);
 	wcd9xxx_disable_supplies(wcd9xxx, pdata);
 	wcd9xxx_device_exit(wcd9xxx);
+	dev_set_drvdata(&client->dev, NULL);
 	return 0;
 }
 
@@ -1846,6 +1848,7 @@ err_supplies:
 	wcd9xxx_disable_supplies(wcd9xxx, pdata);
 err_codec:
 	kfree(wcd9xxx);
+	slim_set_clientdata(slim, NULL);
 err:
 	return ret;
 }
@@ -1864,6 +1867,7 @@ static int wcd9xxx_slim_remove(struct slim_device *pdev)
 	slim_remove_device(wcd9xxx->slim_slave);
 	wcd9xxx_disable_supplies(wcd9xxx, pdata);
 	wcd9xxx_device_exit(wcd9xxx);
+	slim_set_clientdata(pdev, NULL);
 	return 0;
 }
 
