4fd34f83dc790b737b21fefce5eb067ca2f6ae00
Charles Bin <cbin@codeaurora.org>
2014-07-30 14:23:10 -0700

    mdss: mdp: Prevent false null dereference warnings
    Remove null checks that were causing false warnings
    for potential null dereferences in driver.
    
    Change-Id: I3a097c926da47d897e8c8e4fe39808436c413f70
    Signed-off-by: Charles Bin <cbin@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -2065,7 +2065,7 @@ static void mdss_mdp_overlay_pan_display(struct msm_fb_data_type *mfd)
 	u32 offset;
 	int bpp, ret;
 
-	if (!mfd)
+	if (!mfd || !mfd->mdp.private1)
 		return;
 
 	fbi = mfd->fbi;
@@ -3892,6 +3892,8 @@ static int mdss_mdp_overlay_handoff(struct msm_fb_data_type *mfd)
 			goto error;
 		}
 		mdp5_data->ctl = ctl;
+	} else {
+		ctl = mdp5_data->ctl;
 	}
 
 	/*
