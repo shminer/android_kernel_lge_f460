ec1f56e14698cfc7dc6848ac49e57440ffdd1ca7
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2014-11-17 13:22:47 +0530

    msm: mdss: avoid corner cases with DSI_INT_CTRL register read/write
    To enable/disable a particular interrupt mask for DSI, we currently
    read the DSI_INT_CTRL register and add/remove the interrupt mask on
    top of the current register value. With this approach, we sometimes
    clear some interrupts without handling them. Handle this case by
    writing back only the required interrupt mask bits to the
    DSI_INT_CTRL register.
    
    Below is an instance of such issue when a DSI register read
    operation is performed.
    
    <3>[  342.509070] mdss_dsi_isr: ndx=0 isr=3200002
                 -> At first, DSI error interrupt is received.
    
    <3>[  342.512239] mdss_dsi_err_intr_ctrl: intr=1310003 enable=0
                 -> During DSI_INT_CTRL read/write operation,
    we clear the CMD_DMA_DONE interrupt which arrives few milli
    seconds after DSI error interrupt.
    
    <3>[  342.517620] mdss_dsi_fifo_status: status=44441000
    
    <3>[  342.522351] mdss_dsi_timeout_status: status=1
    
    <3>[  342.526980] mdss_dsi_err_intr_ctrl: intr=3210002 enable=1
    
    <3>[  342.693365] mdss_dsi_cmds_rx: failed to tx cmd = 0xa
                 -> This causes a CMD DMA timeout even though the
    CMD_DMA_DONE interrupt arrived.
    
    Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>
    Signed-off-by: Anila kumar Vadla <avadla@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi_host.c
+++ drivers/video/msm/mdss/mdss_dsi_host.c
@@ -456,6 +456,7 @@ void mdss_dsi_err_intr_ctrl(struct mdss_dsi_ctrl_pdata *ctrl, u32 mask,
 	u32 intr;
 
 	intr = MIPI_INP(ctrl->ctrl_base + 0x0110);
+	intr &= DSI_INTR_TOTAL_MASK;
 
 	if (enable)
 		intr |= mask;
@@ -1238,7 +1239,6 @@ static int mdss_dsi_cmd_dma_rx(struct mdss_dsi_ctrl_pdata *ctrl,
 	return rx_byte;
 }
 
-
 void mdss_dsi_wait4video_done(struct mdss_dsi_ctrl_pdata *ctrl)
 {
 	unsigned long flag;
@@ -1246,6 +1246,7 @@ void mdss_dsi_wait4video_done(struct mdss_dsi_ctrl_pdata *ctrl)
 
 	/* DSI_INTL_CTRL */
 	data = MIPI_INP((ctrl->ctrl_base) + 0x0110);
+	data &= DSI_INTR_TOTAL_MASK;
 	data |= DSI_INTR_VIDEO_DONE_MASK;
 
 	MIPI_OUTP((ctrl->ctrl_base) + 0x0110, data);
@@ -1259,6 +1260,7 @@ void mdss_dsi_wait4video_done(struct mdss_dsi_ctrl_pdata *ctrl)
 			msecs_to_jiffies(VSYNC_PERIOD * 4));
 
 	data = MIPI_INP((ctrl->ctrl_base) + 0x0110);
+	data &= DSI_INTR_TOTAL_MASK;
 	data &= ~DSI_INTR_VIDEO_DONE_MASK;
 	MIPI_OUTP((ctrl->ctrl_base) + 0x0110, data);
 }
