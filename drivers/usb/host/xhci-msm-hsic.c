99dc606b8509d0c3a74cde0a7ca096ebd619254b
franciscofranco <franciscofranco.1990@gmail.com>
2015-03-17 16:46:31 +0000

    usb: xhci: disable debugs
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    
    Conflicts:
    	drivers/usb/host/xhci-msm-hsic.c

--- drivers/usb/host/xhci-msm-hsic.c
+++ drivers/usb/host/xhci-msm-hsic.c
@@ -146,8 +146,8 @@ static struct dbg_data dbg_hsic = {
 	.ctrl_lck = __RW_LOCK_UNLOCKED(clck),
 	.data_idx = 0,
 	.data_lck = __RW_LOCK_UNLOCKED(dlck),
-	.log_payload = 1,
-	.log_events = 1,
+	.log_payload = 0,
+	.log_events = 0,
 	.inep_log_mask = 1,
 	.outep_log_mask = 1
 };
