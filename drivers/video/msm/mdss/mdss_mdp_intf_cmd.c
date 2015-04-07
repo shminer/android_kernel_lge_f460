2d92b56490bcc199ea32498eaf24e5e641e0c337
Naseer Ahmed <naseer@codeaurora.org>
2015-04-07 11:29:37 -0700

    msm: mdss: reset MDP ctl path for pingpong timeout recovery
    When pingpong timeout is received, it could be due to some bad
    configuration on MDP pipeline. In order to recover and have clean
    HW for next frames, perform SW reset on the faulting ctl path. Also
    make sure all the pipes in the bad configuration are halted
    properly and does not have any residue transaction for the new
    frames.
    
    Change-Id: I9a6c6cb6d004fd65c24a7fefe7457ee68af9273a
    Signed-off-by: Ujwal Patel <ujwalp@codeaurora.org>
    Signed-off-by: Naseer Ahmed <naseer@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
+++ drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
@@ -586,6 +586,7 @@ static int mdss_mdp_cmd_wait4pingpong(struct mdss_mdp_ctl *ctl, void *arg)
 					rc, ctl->num);
 			MDSS_XLOG_TOUT_HANDLER("mdp", "dsi0", "dsi1",
 					"edp", "hdmi", "panic");
+			mdss_mdp_ctl_reset(ctx->ctl);
 			mdss_fb_send_panel_reset_event(ctl->mfd);
 		}
 		ctx->pp_timeout_report_cnt++;
