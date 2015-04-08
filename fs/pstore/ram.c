371ed20420ed395f0287431c2f1ac5a463922a7a
shminer <332574643@qq.com>
2015-04-08 16:30:22 +0800

    fs:increase pstore record size

--- fs/pstore/ram.c
+++ fs/pstore/ram.c
@@ -44,7 +44,7 @@
 #endif
 
 #define RAMOOPS_KERNMSG_HDR "===="
-#define MIN_MEM_SIZE 4096UL
+#define MIN_MEM_SIZE 8192UL
 
 static ulong record_size = MIN_MEM_SIZE;
 module_param(record_size, ulong, 0400);
