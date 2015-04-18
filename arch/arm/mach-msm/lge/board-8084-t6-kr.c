5012fba83572bd035684372aeaab64298641bdd5
shminer <332574643@qq.com>
2015-04-18 09:11:07 +0800

    fix merge drop

--- arch/arm/mach-msm/lge/board-8084-t6-kr.c
+++ arch/arm/mach-msm/lge/board-8084-t6-kr.c
@@ -64,11 +64,6 @@ void __init apq8084_reserve(void)
 #endif
 }
 
-static void __init apq8084_early_memory(void)
-{
-	of_scan_flat_dt(dt_scan_for_memory_hole, NULL);
-}
-
 #ifdef CONFIG_LGE_LCD_TUNING
 static struct platform_device lcd_misc_device = {
 	.name = "lcd_misc_msm",
@@ -147,11 +142,6 @@ void __init apq8084_init(void)
 	regulator_suppress_info_printing();
 #endif
 	apq8084_add_drivers();
-}
-
-void __init apq8084_init_very_early(void)
-{
-	apq8084_early_memory();
 }
 
 static const char *apq8084_dt_match[] __initconst = {
