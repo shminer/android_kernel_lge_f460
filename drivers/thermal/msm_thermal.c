a291ef33da62c43ee10731de96f21f58a0fac294
Ram Chandrasekar <rkumbako@codeaurora.org>
2015-04-22 11:55:02 -0600

    msm: thermal: Retry hotplug when exiting from suspend
    Update thermal driver to retry hotplug, when
    device exits from suspend. This will ensure that
    any failure to offline/online the cores during
    suspend will be re-applied, when device exits
    suspend.
    
    CRs-Fixed: 823919
    Change-Id: Ifc25ad61288d88f984b18fe51b9dd5776f650cf5
    Signed-off-by: Ram Chandrasekar <rkumbako@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -45,6 +45,7 @@
 #include <linux/msm_thermal_ioctl.h>
 #include <soc/qcom/rpm-smd.h>
 #include <soc/qcom/scm.h>
+#include <linux/suspend.h>
 
 #define MAX_RAILS 5
 #define MAX_THRESHOLD 2
@@ -1225,6 +1226,26 @@ static void __ref do_core_control(long temp)
 	}
 	mutex_unlock(&core_control_mutex);
 }
+
+static int msm_thermal_suspend_callback(
+	struct notifier_block *nfb, unsigned long action, void *data)
+{
+	switch (action) {
+	case PM_POST_HIBERNATION:
+	case PM_POST_SUSPEND:
+		if (hotplug_task)
+			complete(&hotplug_notify_complete);
+		else
+			pr_debug("Hotplug task not initialized\n");
+		break;
+
+	default:
+		return NOTIFY_DONE;
+	}
+
+	return NOTIFY_OK;
+}
+
 /* Call with core_control_mutex locked */
 static int __ref update_offline_cores(int val)
 {
@@ -1252,13 +1273,15 @@ static int __ref update_offline_cores(int val)
 			if (cpu_online(cpu))
 				continue;
 			ret = cpu_up(cpu);
-			if (ret && ret == notifier_to_errno(NOTIFY_BAD))
+			if (ret && ret == notifier_to_errno(NOTIFY_BAD)) {
 				pr_debug("Onlining CPU%d is vetoed\n", cpu);
-			else if (ret)
+			} else if (ret) {
+				cpus_offlined |= BIT(cpu);
 				pr_err("Unable to online CPU%d. err:%d\n",
-						cpu, ret);
-			else
+					cpu, ret);
+			} else {
 				pr_debug("Onlined CPU%d\n", cpu);
+			}
 		}
 	}
 	return ret;
@@ -1295,8 +1318,7 @@ static __ref int do_hotplug(void *data)
 			if (cpus[cpu].offline || cpus[cpu].user_offline)
 				mask |= BIT(cpu);
 		}
-		if (mask != cpus_offlined)
-			update_offline_cores(mask);
+		update_offline_cores(mask);
 		mutex_unlock(&core_control_mutex);
 		sysfs_notify(cc_kobj, NULL, "cpus_offlined");
 	}
@@ -2871,6 +2893,7 @@ int msm_thermal_init(struct msm_thermal_data *pdata)
 	if (ret)
 		pr_err("cannot register cpufreq notifier. err:%d\n", ret);
 
+	pm_notifier(msm_thermal_suspend_callback, 0);
 	INIT_DELAYED_WORK(&check_temp_work, check_temp);
 	schedule_delayed_work(&check_temp_work,
 			msecs_to_jiffies(5000));
