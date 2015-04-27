4573b15aa669e1b540ad7b3925a8aa6d7c0612d5
shminer <332574643@qq.com>
2015-04-27 10:16:19 +0800

    Revert "mmc: core: fix variable being overwritten"
    This reverts commit 69c30c66d8ea1f85f2573a10cebe2c0f6de830a8.

--- drivers/mmc/core/slot-gpio.c
+++ drivers/mmc/core/slot-gpio.c
@@ -22,12 +22,12 @@ struct mmc_gpio {
 	int cd_gpio;
 	char *ro_label;
 	bool status;
-	char cd_label[0]; /* Must be last entry */
+	char cd_label[0];
 };
 
 #ifdef CONFIG_MACH_LGE
-/*
-
+/*           
+                                                   
  */
 int mmc_gpio_get_status(struct mmc_host *host)
 #else
