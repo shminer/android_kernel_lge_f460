82ce41fdf6f2d1893f47fb7fd196deb8f81069da
shminer <332574643@qq.com>
2015-06-01 13:23:24 +0800

    color eggs~

--- drivers/cpufreq/cpufreq_performance.c
+++ drivers/cpufreq/cpufreq_performance.c
@@ -37,7 +37,7 @@ static int cpufreq_governor_performance(struct cpufreq_policy *policy,
 static
 #endif
 struct cpufreq_governor cpufreq_gov_performance = {
-	.name		= "performance",
+	.name		= "武松",
 	.governor	= cpufreq_governor_performance,
 	.owner		= THIS_MODULE,
 };
