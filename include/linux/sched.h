69124085b97076492a0309596a0d10734d1c7867
shminer <a332574643@gmail.com>
2015-11-12 17:51:29 +0800

    alucard hotplug: fix indef for alucard hotplug
    Signed-off-by: shminer <a332574643@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -163,11 +163,8 @@ extern unsigned long nr_running(void);
 extern unsigned long nr_iowait(void);
 extern unsigned long nr_iowait_cpu(int cpu);
 extern unsigned long this_cpu_load(void);
-#ifdef CONFIG_INTELLI_HOTPLUG
 extern unsigned long avg_nr_running(void);
 extern unsigned long avg_cpu_nr_running(unsigned int cpu);
-#endif
-
 
 extern void calc_global_load(unsigned long ticks);
 extern void update_cpu_load_nohz(void);
