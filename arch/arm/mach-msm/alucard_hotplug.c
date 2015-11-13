ded03d35ed7200e3f3fb9a0790a57406b94fbb26
shminer <a332574643@gmail.com>
2015-11-13 12:32:08 +0800

    alucard hotplug: tweak

--- arch/arm/mach-msm/alucard_hotplug.c
+++ arch/arm/mach-msm/alucard_hotplug.c
@@ -738,9 +738,9 @@ static int __init alucard_hotplug_init(void)
 	};
 	unsigned int hotplug_rq[NR_CPUS][2] = {
 		{0, 150},
-		{150, 300},
-		{300, 450},
-		{450, 0}
+		{150, 750},
+		{750, 850},
+		{850, 0}
 	};
 	unsigned int hotplug_rate[NR_CPUS][2] = {
 		{1, 1},
