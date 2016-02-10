afef57aed72327a14036027bee7f6eca7ea50e56
shminer <a332574643@gmail.com>
2016-02-10 11:01:47 +0800

    Revert "input: touchboost: conservative samples every 10ms, no point in limiting input_time to update its value in 50ms intervals"
    This reverts commit 47d1cc166365e91a926a683c1c5092f9dc3ec051.

--- drivers/input/touchscreen/touchboost.c
+++ drivers/input/touchscreen/touchboost.c
@@ -21,6 +21,8 @@
 #include <linux/time.h>
 #include <linux/slab.h>
 
+#define MIN_TIME_INTERVAL_US (50 * USEC_PER_MSEC)
+
 struct touchboost_inputopen {
 	struct input_handle *handle;
 	struct work_struct inputopen_work;
@@ -41,8 +43,16 @@ inline u64 get_input_time(void)
 static void boost_input_event(struct input_handle *handle,
                 unsigned int type, unsigned int code, int value)
 {
-	if ((type == EV_ABS))
+	u64 now;
+
+	if ((type == EV_ABS)) {
+		now = ktime_to_us(ktime_get());
+
+		if (now - last_input_time < MIN_TIME_INTERVAL_US)
+			return;
+
 		last_input_time = ktime_to_us(ktime_get());
+	}
 }
 
 static int boost_input_connect(struct input_handler *handler,
