10e571f55e4c0750ab73f4d9a2a5389591952257
shminer <a332574643@gmail.com>
2016-03-15 08:20:37 +0800

    qcom: add last state notifier, update msm limiter

--- include/soc/qcom/limiter.h
+++ include/soc/qcom/limiter.h
@@ -1,35 +1,46 @@
-#define MSM_LIMIT			"msm_limiter"
+#define MSM_LIMITER			"msm_limiter"
 #define LIMITER_ENABLED			1
-#define DEFAULT_SUSPEND_DEFER_TIME	10
-#define DEFAULT_SUSPEND_FREQUENCY	1497600
-
+#define MPD_ENABLED			0
+#if defined(CONFIG_ARCH_MSM8916)
+#define DEFAULT_SUSPEND_FREQUENCY	998400
+#else
+#define DEFAULT_SUSPEND_FREQUENCY	1728000
+#endif
 #if defined(CONFIG_ARCH_APQ8084)
 #define DEFAULT_RESUME_FREQUENCY	2649600
+#elif defined(CONFIG_ARCH_MSM8916)
+#define DEFAULT_RESUME_FREQUENCY	1209600
 #else
 #define DEFAULT_RESUME_FREQUENCY	2265600
 #endif
+#if defined(CONFIG_ARCH_MSM8916)
+#define DEFAULT_MIN_FREQUENCY		200000
+#else
 #define DEFAULT_MIN_FREQUENCY		300000
+#endif
 
 static struct cpu_limit {
 	unsigned int limiter_enabled;
+	unsigned int mpd_enabled;
 	uint32_t suspend_max_freq;
-	uint32_t limit_max_freq;
-	uint32_t limit_min_freq;
-	uint32_t limit_max_ori;
-	uint32_t limit_min_ori;
-	unsigned int suspended;
-	unsigned int suspend_defer_time;
-	struct delayed_work suspend_work;
-	struct work_struct resume_work;
-	struct mutex resume_suspend_mutex;
+	uint32_t resume_max_freq_all;
+	uint32_t resume_max_freq[4];
+	uint32_t suspend_min_freq_all;
+	uint32_t suspend_min_freq[4];
 	struct mutex msm_limiter_mutex[4];
 	struct notifier_block notif;
 } limit = {
 	.limiter_enabled = LIMITER_ENABLED,
+	.mpd_enabled = MPD_ENABLED,
 	.suspend_max_freq = DEFAULT_SUSPEND_FREQUENCY,
-	.limit_max_ori = DEFAULT_RESUME_FREQUENCY,
-	.limit_min_ori = DEFAULT_MIN_FREQUENCY,
-	.limit_max_freq = DEFAULT_RESUME_FREQUENCY,
-	.limit_min_freq = DEFAULT_MIN_FREQUENCY,
-	.suspend_defer_time = DEFAULT_SUSPEND_DEFER_TIME,
+	.resume_max_freq_all = DEFAULT_RESUME_FREQUENCY,
+	.resume_max_freq[0] = DEFAULT_RESUME_FREQUENCY,
+	.resume_max_freq[1] = DEFAULT_RESUME_FREQUENCY,
+	.resume_max_freq[2] = DEFAULT_RESUME_FREQUENCY,
+	.resume_max_freq[3] = DEFAULT_RESUME_FREQUENCY,
+	.suspend_min_freq_all = DEFAULT_MIN_FREQUENCY,
+	.suspend_min_freq[0] = DEFAULT_MIN_FREQUENCY,
+	.suspend_min_freq[1] = DEFAULT_MIN_FREQUENCY,
+	.suspend_min_freq[2] = DEFAULT_MIN_FREQUENCY,
+	.suspend_min_freq[3] = DEFAULT_MIN_FREQUENCY,
 };
