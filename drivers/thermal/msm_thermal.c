48c704c6a00f6a65c0652ab9e8aeee5052f26310
shminer <a332574643@gmail.com>
2016-03-17 10:58:30 +0800

    thermal: adjust thermal

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -75,9 +75,9 @@ static struct msm_thermal_data msm_thermal_info;
 static struct msm_thermal_data_intelli msm_thermal_info_local = {
 	.sensor_id = 5,
 	.poll_ms = DEFAULT_POLLING_MS,
-	.limit_temp_degC = 60,
+	.limit_temp_degC = 52,
 	.temp_hysteresis_degC = 5,
-	.limit_safe_temp_degC = 75,
+	.limit_safe_temp_degC = 60,
 	.freq_step = 2,
 	.freq_control_mask = 0xf,
 	.core_limit_temp_degC = 80,
