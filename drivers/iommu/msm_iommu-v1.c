ec2b2cebbc63bda718b154371a9c1f74cf635734
shminer <a332574643@gmail.com>
2016-03-28 08:25:07 +0800

    Revert "iommu: msm: do tight polling for TLBSTATUS checking"
    This reverts commit fdef8856d20b69a26258910f4a7d5a92393c8c55.

--- drivers/iommu/msm_iommu-v1.c
+++ drivers/iommu/msm_iommu-v1.c
@@ -359,21 +359,15 @@ static void __sync_tlb(struct msm_iommu_drvdata *iommu_drvdata, int ctx,
 		struct msm_iommu_priv *priv)
 {
 	unsigned int val;
-	unsigned int count;
+	unsigned int res;
 	void __iomem *base = iommu_drvdata->cb_base;
 
 	SET_TLBSYNC(base, ctx, 0);
 	/* No barrier needed due to read dependency */
 
-	for (count = 500000; count > 0; count--) {
-		val = readl_relaxed(CTX_REG(CB_TLBSTATUS, base, ctx));
-		if ((val & CB_TLBSTATUS_SACTIVE) == 0)
-			break;
-
-		udelay(1);
-	}
-
-	if (!count)
+	res = readl_poll_timeout_noirq(CTX_REG(CB_TLBSTATUS, base, ctx), val,
+				(val & CB_TLBSTATUS_SACTIVE) == 0, 10000, 50);
+	if (res)
 		check_tlb_sync_state(iommu_drvdata, ctx, priv);
 }
 
