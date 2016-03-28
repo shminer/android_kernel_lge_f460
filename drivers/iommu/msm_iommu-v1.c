6fb777d90e4f4ad961c1cd27956d13cf612b90e8
shminer <a332574643@gmail.com>
2016-03-28 08:25:13 +0800

    Revert "iommu: msm: consider only fault indicators from FSR"
    This reverts commit a001cff252552e802ee31f5e807bb1088bb11062.

--- drivers/iommu/msm_iommu-v1.c
+++ drivers/iommu/msm_iommu-v1.c
@@ -1413,7 +1413,7 @@ irqreturn_t msm_iommu_fault_handler_v2(int irq, void *dev_id)
 	}
 
 	fsr = GET_FSR(drvdata->cb_base, ctx_drvdata->num);
-	if (fsr & 0x1FF) {
+	if (fsr) {
 		if (!ctx_drvdata->attached_domain) {
 			pr_err("Bad domain in interrupt handler\n");
 			ret = -ENOSYS;
