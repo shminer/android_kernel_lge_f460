3d9fb3366245530271737adb46d30a72cba21550
shminer <a332574643@gmail.com>
2016-04-11 08:01:33 +0800

    Revert "sched/bfs: Remove false-positive warning from wake_up_process()"
    This reverts commit 8b8b82c20d08630dfbcb157993a9df2969bda9ec.

--- kernel/sched/bfs.c
+++ kernel/sched/bfs.c
@@ -1643,6 +1643,7 @@ static void try_to_wake_up_local(struct task_struct *p)
  */
 int wake_up_process(struct task_struct *p)
 {
+	WARN_ON(task_is_stopped_or_traced(p));
 	return try_to_wake_up(p, TASK_NORMAL, 0);
 }
 EXPORT_SYMBOL(wake_up_process);
